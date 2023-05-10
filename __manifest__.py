bc567c62b739552766a431a1662055aca739aded
Sergey Linnik <sergey.linnik@onlyoffice.com>
2023-05-10 14:08:00 +0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "ONLYOFFICE Connector for Odoo",
+    'name': "ONLYOFFICE",
 
     'summary': "Edit and collaborate on office files within Odoo Documents.",
 
@@ -9,11 +9,8 @@
     'author': "ONLYOFFICE",
     'website': "https://www.onlyoffice.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Productivity',
-    'version': '0.1',
+    'version': '1.0.0',
 
     'depends': ['base', 'mail'],
 
@@ -26,6 +23,7 @@
     ],
 
     'license': 'AGPL-3',
+    'support': 'support@onlyoffice.com',
 
     'installable': True,
     'application': True,
