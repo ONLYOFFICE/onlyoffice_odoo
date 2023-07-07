436ddc3de8b259f96a52cf503e72449f9546e57f
Andrei Panov <andrew.panov@onlyoffice.com>
2023-07-07 14:00:40 +0000

    read document template method

--- onlyoffice_odoo/utils/file_utils.py
+++ onlyoffice_odoo/utils/file_utils.py
@@ -29,33 +29,9 @@
 # terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 #
 
-from odoo.addons.onlyoffice_odoo.utils import format_utils
+import os
 
-locale_path = {
-    "az": "az-Latn-AZ",
-    "bg": "bg-BG",
-    "cs": "cs-CZ",
-    "de": "de-DE",
-    "el": "el-GR",
-    "en-gb": "en-GB",
-    "en": "en-US",
-    "es": "es-ES",
-    "fr": "fr-FR",
-    "it": "it-IT",
-    "ja": "ja-JP",
-    "ko": "ko-KR",
-    "lv": "lv-LV",
-    "nl": "nl-NL",
-    "pl": "pl-PL",
-    "pt-br": "pt-BR",
-    "pt": "pt-PT",
-    "ru": "ru-RU",
-    "sk": "sk-SK",
-    "sv": "sv-SE",
-    "uk": "uk-UA",
-    "vi": "vi-VN",
-    "zh": "zh-CN",
-}
+from odoo.addons.onlyoffice_odoo.utils import format_utils
 
 
 def get_file_title_without_ext(name):
@@ -128,3 +104,57 @@ def get_default_name_by_type(str):
         return "Form template"
 
     return None
+
+def get_mime_by_ext(str):
+    if str == "docx":
+        return "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    if str == "xlsx":
+        return "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    if str == "pptx":
+        return "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+
+    return None
+
+def get_default_file_template(lang, ext):
+    locale_path = {
+        "az": "az-Latn-AZ",
+        "bg": "bg-BG",
+        "cs": "cs-CZ",
+        "de": "de-DE",
+        "el": "el-GR",
+        "en-gb": "en-GB",
+        "en": "en-US",
+        "es": "es-ES",
+        "fr": "fr-FR",
+        "it": "it-IT",
+        "ja": "ja-JP",
+        "ko": "ko-KR",
+        "lv": "lv-LV",
+        "nl": "nl-NL",
+        "pl": "pl-PL",
+        "pt-br": "pt-BR",
+        "pt": "pt-PT",
+        "ru": "ru-RU",
+        "sk": "sk-SK",
+        "sv": "sv-SE",
+        "uk": "uk-UA",
+        "vi": "vi-VN",
+        "zh": "zh-CN",
+    }
+
+    lang = lang.replace("_", "-")
+
+    locale = locale_path.get(lang)
+    if locale is None:
+        lang = lang.split("-")[0]
+        locale = locale_path.get(lang)
+        if locale is None:
+            locale = locale_path.get("en")
+
+    file = open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "static", "assets", "document_templates", locale, "new." + ext), "rb")
+
+    try:
+        file_data = file.read()
+        return file_data
+    finally:
+        file.close()
