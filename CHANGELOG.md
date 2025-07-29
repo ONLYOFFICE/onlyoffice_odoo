149db928d7fffe894a8c8a527809e472f20bc808
Sergey Linnik <sergey.linnik@onlyoffice.com>
2025-07-29 15:45:30 +0300

    5.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Change Log
 
-##
+## 5.2.0
 
 ## Changed
 
--- onlyoffice_odoo/__manifest__.py
+++ onlyoffice_odoo/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "ONLYOFFICE",
     "website": "https://github.com/ONLYOFFICE/onlyoffice_odoo",
     "category": "Productivity",
-    "version": "5.1.0",
+    "version": "5.2.0",
     "depends": ["base", "mail"],
     "external_dependencies": {"python": ["pyjwt"]},
     # always loaded
