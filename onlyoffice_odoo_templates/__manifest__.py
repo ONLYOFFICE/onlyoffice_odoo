ce6ac438e9346177aa95af444ec20a17f81bf9d5
Sergey Linnik <sergey.linnik@onlyoffice.com>
2025-09-24 17:47:34 +0300

    templates: 2.3.2

--- onlyoffice_odoo_templates/__manifest__.py
+++ onlyoffice_odoo_templates/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "ONLYOFFICE",
     "website": "https://github.com/ONLYOFFICE/onlyoffice_odoo",
     "category": "Productivity",
-    "version": "2.2.2",
+    "version": "2.3.2",
     "depends": ["base", "onlyoffice_odoo", "web"],
     "external_dependencies": {"python": ["pyjwt"]},
     # always loaded
