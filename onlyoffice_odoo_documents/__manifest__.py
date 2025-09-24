faed90a80e87af0d92c0da7f57991d72fc229fa3
Sergey Linnik <sergey.linnik@onlyoffice.com>
2025-09-24 17:41:50 +0300

    documents: 3.2.1

--- onlyoffice_odoo_documents/__manifest__.py
+++ onlyoffice_odoo_documents/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "ONLYOFFICE",
     "website": "https://github.com/ONLYOFFICE/onlyoffice_odoo",
     "category": "Productivity",
-    "version": "3.1.1",
+    "version": "3.2.1",
     "depends": ["onlyoffice_odoo", "documents"],
     # always loaded
     "data": ["security/ir.model.access.csv"],
