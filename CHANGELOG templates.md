59d8bfcf2554056dba99e72b015c548746ccd4ee
Sergey Linnik <sergey.linnik@onlyoffice.com>
2025-09-24 17:44:48 +0300

    templates: 1.3.2

--- CHANGELOG templates.md
+++ CHANGELOG templates.md
@@ -1,5 +1,7 @@
 # Change Log
 
+## 1.3.2
+
 ## Added
 
 - setting to disable form fields editing
--- onlyoffice_odoo_templates/__manifest__.py
+++ onlyoffice_odoo_templates/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "ONLYOFFICE",
     "website": "https://github.com/ONLYOFFICE/onlyoffice_odoo",
     "category": "Productivity",
-    "version": "1.2.2",
+    "version": "1.3.2",
     "depends": ["base", "onlyoffice_odoo", "web"],
     "external_dependencies": {"python": ["pyjwt"]},
     # always loaded
