7d966d1019d3ff97cd9f2486105adbd36ae8f68e
Sergey Linnik <sergey.linnik@onlyoffice.com>
2025-09-24 17:49:19 +0300

    documents: 5.2.1

--- CHANGELOG documents.md
+++ CHANGELOG documents.md
@@ -1,5 +1,7 @@
 # Change Log
 
+## 5.2.1
+
 ## Added
 
 - form gallery
--- onlyoffice_odoo_documents/__manifest__.py
+++ onlyoffice_odoo_documents/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "ONLYOFFICE",
     "website": "https://github.com/ONLYOFFICE/onlyoffice_odoo",
     "category": "Productivity",
-    "version": "5.1.1",
+    "version": "5.2.1",
     "depends": ["onlyoffice_odoo", "documents"],
     # always loaded
     "data": [
