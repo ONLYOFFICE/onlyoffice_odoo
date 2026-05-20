cf2685e068dde9d2213b3bac6e35cb4cfd5d47b6
ZEROM22 <alexander.salyakhov@onlyoffice.com>
2026-05-20 17:21:01 +0300

    feat: copy the key if the Automation API is unavailable

--- onlyoffice_odoo_templates/static/src/views/editor/onlyoffice_editor.js
+++ onlyoffice_odoo_templates/static/src/views/editor/onlyoffice_editor.js
@@ -18,12 +18,12 @@ class TemplateEditor extends Component {
     this.notificationService = useService("notification")
     this.router = useService("router")
 
-    this.state = useState({ resModel: "" })
+    this.state = useState({ resModel: "", hasLicense: false })
 
     this.config = null
     this.docApiJS = null
     this.documentReady = false
-    this.hasLicense = false
+    this.noLicenseNotified = false
     this.script = null
     this.unchangedModels = {}
 
@@ -49,12 +49,13 @@ class TemplateEditor extends Component {
             if (window.docEditor && "createConnector" in window.docEditor) {
               window.connector = docEditor.createConnector()
               window.connector.executeMethod("GetVersion", [], () => {
-                this.hasLicense = true
+                this.state.hasLicense = true
               })
             }
             // Render fields
             this.state.resModel = template_model_model
             this.documentReady = true
+            setTimeout(() => this.showNoLicenseNotification(), 1500)
           },
         }
         const theme = cookie.get("color_scheme")
@@ -108,10 +109,25 @@ class TemplateEditor extends Component {
     })
   }
 
+  showNoLicenseNotification() {
+    if (!this.state.hasLicense && !this.noLicenseNotified) {
+      this.noLicenseNotified = true
+      this.notificationService.add(
+        _t("Note: The ONLYOFFICE Automation API is not activated in your instance, so automatic insertion of predefined keys from Odoo into the ONLYOFFICE editor isn't available. You can manually create the field and paste the key from your clipboard."),
+        {
+          type: "warning",
+          sticky: true,
+        },
+      )
+    }
+  }
+
   createForm(field) {
     if (this.documentReady) {
-      if (!this.hasLicense) {
-        this.notificationService.add(_t("Couldn't insert the field. Please check Automation API."), { type: "danger" })
+      if (!this.state.hasLicense) {
+        const key = field.id.replaceAll("/", " ")
+        navigator.clipboard.writeText(key)
+        this.notificationService.add(_t("Key copied to clipboard: %s", key), { type: "success" })
         return
       }
       Asc.scope.data = field
--- onlyoffice_odoo_templates/static/src/views/editor/onlyoffice_editor_export_data.js
+++ onlyoffice_odoo_templates/static/src/views/editor/onlyoffice_editor_export_data.js
@@ -197,5 +197,8 @@ ExportData.components = {
   CheckBox,
   ExportDataItem,
 }
-ExportData.props = { resModel: String }
+ExportData.props = {
+  hasLicense: Boolean,
+  resModel: String,
+}
 ExportData.template = "onlyoffice_odoo_templates.ExportData"
