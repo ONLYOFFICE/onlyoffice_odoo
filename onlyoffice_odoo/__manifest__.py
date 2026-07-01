0af46e9a02dd44a8d1008c61d73c19b547aaaa8d
Sergey Linnik <sergey.linnik@onlyoffice.com>
2026-07-01 14:27:07 +0300

    4.4.0

--- onlyoffice_odoo/__manifest__.py
+++ onlyoffice_odoo/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "ONLYOFFICE",
     "website": "https://www.onlyoffice.com/office-for-odoo?utm_source=odoo_market",
     "category": "Productivity",
-    "version": "4.3.0",
+    "version": "4.4.0",
     "license": "LGPL-3",
     "support": "support@onlyoffice.com",
     "depends": ["base", "mail"],
